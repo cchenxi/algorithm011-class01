b751ded27a1be45388af8e07c23d0a7a60fe30c1
chenxi <chenxi.et@gmail.com>
2020-06-21 22:07:05 +0800

    初始化项目 && 添加junit依赖

---
+++ Week_01/src/test/java/com/cchenxi/w1/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package com.cchenxi.w1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 测试单元测试
+ * Date: 2020-06-21
+ *
+ * @author chenxi
+ */
+public class HelloWorldTest {
+    @Test
+    public void test() {
+        Assert.assertEquals(1, 1);
+    }
+}
