2352399b7bf189eabc296b9e05582362397363b2
chenxi <chenxi.et@gmail.com>
2020-07-11 22:13:18 +0800

    three sum

--- Week_01/src/main/java/com/cchenxi/w1/practice/n15/ThreeSum.java
+++ Week_01/src/main/java/com/cchenxi/w1/practice/n15/ThreeSum.java
@@ -3,8 +3,11 @@ package com.cchenxi.w1.practice.n15;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -34,7 +37,7 @@ import java.util.Set;
  */
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
-        return m1(nums);
+        return m2(nums);
     }
 
     /**
@@ -51,6 +54,7 @@ public class ThreeSum {
             return Collections.emptyList();
         }
 
+        //暴力解法 这边可以不用sort
         Arrays.sort(nums);
         Set<List<Integer>> result = new LinkedHashSet<>();
         for (int i = 0; i < nums.length - 2; i++) {
@@ -59,6 +63,30 @@ public class ThreeSum {
                     if (nums[i] + nums[j] + nums[k] == 0) {
                         result.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     }
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+    public List<List<Integer>> m2(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return Collections.emptyList();
+        }
+        Set<List<Integer>> result = new LinkedHashSet<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            //two sum solution
+            int target = -nums[i];
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int j = i + 1; j < nums.length; j++) {
+                int other = target - nums[j];
+                Integer exist = map.get(other);
+                if (exist != null) {
+                    List<Integer> list = Arrays.asList(nums[i], exist, nums[j]);
+                    list.sort(Comparator.naturalOrder());
+                    result.add(list);
+                } else {
+                    map.put(nums[j], nums[j]);
                 }
             }
         }
