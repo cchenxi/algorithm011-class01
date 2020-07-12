a8fc01a5804ea0584941c90d10c20facb5a0baf7
chenxi <chenxi.et@gmail.com>
2020-07-12 13:45:22 +0800

    生成括号

---
+++ Week_03/src/test/java/com/cchenxi/w3/n22/GenerateParenthesesTest.java
@@ -0,0 +1,22 @@
+package com.cchenxi.w3.n22;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Date: 2020-07-09
+ *
+ * @author chenxi
+ */
+public class GenerateParenthesesTest {
+
+    @Test
+    public void generateParenthesis() {
+        GenerateParentheses c = new GenerateParentheses();
+        List<String> result = c.generateParenthesis(3);
+        System.out.println(1);
+    }
+}
