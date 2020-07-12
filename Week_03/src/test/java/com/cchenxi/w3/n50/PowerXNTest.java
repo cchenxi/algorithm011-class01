41de6be62e05f8cd5f1f95cb65946ba139c715e8
chenxi <chenxi.et@gmail.com>
2020-07-12 20:24:24 +0800

    pow(x, n)

---
+++ Week_03/src/test/java/com/cchenxi/w3/n50/PowerXNTest.java
@@ -0,0 +1,19 @@
+package com.cchenxi.w3.n50;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Date: 2020-07-12
+ *
+ * @author chenxi
+ */
+public class PowerXNTest {
+
+    @Test
+    public void myPow() {
+        PowerXN c = new PowerXN();
+        System.out.println(c.myPow(2, -1));
+    }
+}
