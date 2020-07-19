25e3da5de6d2a574606db97f0ad1a0f719ba5481
chenxi <chenxi.et@gmail.com>
2020-07-19 15:57:12 +0800

    week-04 岛屿数量

---
+++ Week_04/src/test/java/com/cchenxi/w4/n200/NumberOfIslandsTest.java
@@ -0,0 +1,34 @@
+package com.cchenxi.w4.n200;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Date: 2020-07-19
+ *
+ * @author chenxi
+ */
+public class NumberOfIslandsTest {
+    private NumberOfIslands numberOfIslands;
+    private final char[][] array = new char[][]
+            {
+                    {'1','1','0','0','0'},
+                    {'1','1','0','0','0'},
+                    {'0','0','1','0','0'},
+                    {'0','0','0','1','1'}
+            };
+
+    @Before
+    public void setUp() throws Exception {
+        numberOfIslands = new NumberOfIslands();
+    }
+
+    @Test
+    public void numIslands() {
+        NumberOfIslands numberOfIslands = new NumberOfIslands();
+        int nums = numberOfIslands.numIslands(array);
+        System.out.println(nums);
+    }
+}
